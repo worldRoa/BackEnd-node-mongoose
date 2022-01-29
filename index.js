211aa78ed9424a91aaa6eb95b12037407bca2c0d
worldroa <worldraod@gmail.com>
2022-01-28 18:01:41 -0600

    Mongoose Part 3

--- index.js
+++ index.js
@@ -18,10 +18,26 @@ connect.then(() => {
     })
     .then(campsite => {
         console.log(campsite);
-        return Campsite.find();
+
+        return Campsite.findByIdAndUpdate(campsite_id, {
+            $set: { description: 'Updated Test Document'}
+        }, {
+            new: true
+        });
     })
-    .then(campsites => {
-        console.log(campsites);
+    .then(campsite => {
+        console.log(campsite);
+
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificent view!',
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save();
+    })
+    .then(campsite => {
+        console.log(campsite);
         return Campsite.deleteMany();
     })
     .then(() => {
--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
+
 const campsiteSchema = new Schema({
     name: {
         type: String,
